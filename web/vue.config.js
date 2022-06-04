51629f25f9730a20deab8c161c2ccd81db65e690
Anne Haley <anne.haley@kitware.com>
2022-06-04 12:54:44 -0400

    Add Vuetify

--- web/vue.config.js
+++ web/vue.config.js
@@ -2,8 +2,6 @@ module.exports = {
   lintOnSave: false,
 
   transpileDependencies: [
-    // If babel ends up being required, this line is necessary. Without it,
-    //  it has no effect.
     'vuetify',
   ],
 
